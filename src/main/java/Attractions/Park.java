dd6d38004f04b926e48a64d322159d826c48e6c7
Can Toraman <cantoraman@gmail.com>
2018-07-02 21:57:12 +0100

    a commit with a question in theme park test 2

--- src/main/java/Attractions/Park.java
+++ src/main/java/Attractions/Park.java
@@ -6,7 +6,7 @@ import Interfaces.IReviewed;
 
 import java.util.Collection;
 
-public class Park extends Attraction implements IControl, Collection<IReviewed> {
+public class Park extends Attraction implements IControl {
 
     public Park(String name){
         super(name);
