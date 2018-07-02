71b536d4f1de89759cca8e98c5cfa5f4304d23e8
Can Toraman <cantoraman@gmail.com>
2018-07-02 16:08:25 +0100

    initializes stall abstract and its inheritors

--- src/main/java/Stalls/CandyFlossStall.java
+++ src/main/java/Stalls/CandyFlossStall.java
@@ -1,4 +1,8 @@
 package Stalls;
 
-public class CandyFlossStall {
+public class CandyFlossStall extends Stall {
+
+    public CandyFlossStall(String name, String owner, String spot){
+        super(name, owner, spot);
+    }
 }
--- src/main/java/Stalls/TobaccoStall.java
+++ src/main/java/Stalls/TobaccoStall.java
@@ -1,4 +1,8 @@
 package Stalls;
 
-public class TobaccoStall {
+public class TobaccoStall extends Stall {
+    public TobaccoStall(String name, String owner, String spot) {
+        super(name, owner, spot);
+    }
+
 }
